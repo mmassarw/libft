f2526744af1daee238982edad672f56eb8a62a63
Mohammad Massarweh <mmassarw@lab2r3s3.42abudhabi.ae>
2023-01-10 01:51:32 +0400

    Misc. changes

--- Makefile
+++ Makefile
@@ -6,28 +6,37 @@
 #    By: mmassarw <mmassarw@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/06 19:58:12 by mmassarw          #+#    #+#              #
-#    Updated: 2023/01/07 05:27:23 by mmassarw         ###   ########.fr        #
+#    Updated: 2023/01/10 01:51:03 by mmassarw         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME	= libft.a
-CC	= @gcc
-CFLAGS	= -Wall -Wextra -Werror
-RM	= @rm -f
+NAME	=	libft.a
 
-SRCS	= $(wildcards *.c)
+CC		=	@gcc
+CFLAGS	=	-Wall -Wextra -Werror
 
-OBJS	= $(patsubst %.c,%.o,$(wildcard *.c))
+RM		=	@rm -f
+
+ECHO	=	@echo
+
+SRCS	=	$(wildcards *.c)
+
+OBJS	=	$(patsubst %.c,%.o,$(wildcard *.c))
 
 $(NAME):	$(OBJS)
+			$(ECHO) "Compiling libft .........."
 			@ar -rcs $(NAME) $(OBJS)
 
 all:		$(NAME)
 
 clean:
+			$(ECHO) "Removing obj files ......."
 			$(RM) $(OBJS)
 
 fclean:		clean
+			$(ECHO) "Removing libft.a files ..."
 			$(RM) $(NAME)
 
-re:		fclean all
+re:			fclean all
+
+.PHONY: all clean fclean re
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmassarw <mmassarw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:32:13 by mmassarw          #+#    #+#             */
-/*   Updated: 2023/01/07 06:30:46 by mmassarw         ###   ########.fr       */
+/*   Updated: 2023/01/10 01:45:52 by mmassarw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,41 +28,73 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+//		=================		//
+//		is		functions
+//		=================		//
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
+
+//		=================		//
+//		mem		functions
+//		=================		//
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+//		=================		//
+//		str		functions
+//		=================		//
+size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t n);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *nptr);
-void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+
+//		=================		//
+//		to		functions
+//		=================		//
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+//		=================		//
+//		conv	functions
+//		=================		//
+int		ft_atoi(const char *nptr);
+char	*ft_itoa(int n);
+
+//		=================		//
+//		split	functions
+//		=================		//
 char	**ft_split(char const *s, char c);
 void	ft_free_split(char **split);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+//		=================		//
+//		put		functions
+//		=================		//
 int		ft_putchar_fd(char c, int fd);
 int		ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+//		=================		//
+//		list	functions
+//		=================		//
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
@@ -72,7 +104,15 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+//		=================		//
+//		gnl		functions
+//		=================		//
 char	*get_next_line(int fd);
+
+//		=================		//
+//		printf	functions
+//		=================		//
 int		ft_printf(const char *arg, ...);
 
 #endif
